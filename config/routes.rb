ca7e208ff5317478b7590d2c3343215179f36123
grandtheft-spaceship <justdizon71@gmail.com>
2016-02-12 00:24:58 +0000

    Added Student Enrollment feature and validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create 
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
@@ -10,6 +12,10 @@ Flixter::Application.routes.draw do
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
+  end
+
+    resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
